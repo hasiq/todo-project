6ad08de284b383d74e2a3d32777fcc49c0358030
hasiq <jhasikowski@gmail.com>
2024-07-06 10:53:21 +0200

    added another bean to logicConfiguration

--- todo-app/src/main/java/io/github/hasiq/logic/LogicConfiguration.java
+++ todo-app/src/main/java/io/github/hasiq/logic/LogicConfiguration.java
@@ -3,6 +3,7 @@ package io.github.hasiq.logic;
 import io.github.hasiq.TaskConfigurationPropertries;
 import io.github.hasiq.model.ProjectRepository;
 import io.github.hasiq.model.TaskGroupRepository;
+import io.github.hasiq.model.TaskRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,5 +15,13 @@ public class LogicConfiguration {
                                   final TaskConfigurationPropertries taskConfigurationPropertries
     ) {
         return new ProjectService(projectRepository, taskGroupRepository,taskConfigurationPropertries);
+    }
+
+    @Bean
+    TaskGroupService taskGroupService(
+            final TaskGroupRepository taskGroupRepository,
+            final TaskRepository taskRepository
+    ){
+        return new TaskGroupService(taskGroupRepository,taskRepository);
     }
 }
