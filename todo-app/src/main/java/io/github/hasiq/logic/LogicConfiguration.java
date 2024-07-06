3951b64feef9071ab492029d3147d42636da10b6
hasiq <jhasikowski@gmail.com>
2024-07-06 10:17:22 +0200

    added bean class instead of service annotation

---
+++ todo-app/src/main/java/io/github/hasiq/logic/LogicConfiguration.java
@@ -0,0 +1,19 @@
+package io.github.hasiq.logic;
+
+import io.github.hasiq.TaskConfigurationPropertries;
+import io.github.hasiq.model.Project;
+import io.github.hasiq.model.ProjectRepository;
+import io.github.hasiq.model.TaskGroupRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LogicConfiguration {
+    @Bean
+    ProjectService projectService(final ProjectRepository projectRepository,
+                                  final TaskGroupRepository taskGroupRepository,
+                                  final TaskConfigurationPropertries taskConfigurationPropertries
+    ) {
+        return new ProjectService(projectRepository, taskGroupRepository,taskConfigurationPropertries);
+    }
+}
