c67b4d2eb15b10acf0227da9fe55b35836446afb
hasiq <jhasikowski@gmail.com>
2024-07-08 17:45:02 +0200

    added test

---
+++ todo-app/src/test/java/io/github/hasiq/controller/TaskControllerLightIntegrationTest.java
@@ -0,0 +1,42 @@
+package io.github.hasiq.controller;
+
+import io.github.hasiq.model.Task;
+import io.github.hasiq.model.TaskRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.contains;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@WebMvcTest(TaskController.class)
+class TaskControllerLightIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private TaskRepository repo;
+
+    @Test
+    void httpGet_returnsGivenTask() throws Exception {
+        //given
+        String description = "foo";
+        when(repo.findById(anyInt()))
+                .thenReturn(Optional.of(new Task("foo", LocalDateTime.now())));
+
+        //when + then
+        mockMvc.perform(get("/tasks/1231231"))
+                .andDo(print())
+                .andExpect(content().string(containsString(description)));
+    }
+}
